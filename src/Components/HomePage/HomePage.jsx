5ca5e8c002060e64703676f97be636d59657bafc
Suraj Chhipa <SurajChhipa092002@gmail.com>
2023-07-23 14:30:34 +0530

    Hover Effeect Add

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -2,26 +2,29 @@ import React from "react";
 import "./HomePage.css";
 import "./HomePageMd.css";
 import { Icon } from "../../Constant/IconPath";
+import { NavLink } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
       <div className="Hero_Page_Container">
-          <div className="Hero_left_Container">
-            <div className="Hero_Heading">
-              Great software is built with<span> amazing developers</span>
-            </div>
-            <div className="Hero_Title">
-              We help build and manage a team of world-class developers to bring
-              your vision to life
-            </div>
-            <div className="Hero_Button">
-              <button className="HeroBtn">Get Start</button>
-            </div>
+        <div className="Hero_left_Container">
+          <div className="Hero_Heading">
+            Great software is built with<span> amazing developers</span>
           </div>
-          <div className="Hero_Right_Container">
-            <img src={Icon.MainChair_LOgo} alt="" className="MainImg" />
+          <div className="Hero_Title">
+            We help build and manage a team of world-class developers to bring
+            your vision to life
           </div>
+          <div className="Hero_Button">
+            {/* <NavLink to="/about"> */}
+            <button className="HeroBtn">Get Start</button>
+            {/* </NavLink> */}
+          </div>
+        </div>
+        <div className="Hero_Right_Container">
+          <img src={Icon.MainChair_LOgo} alt="" className="MainImg" />
+        </div>
       </div>
     </>
   );
