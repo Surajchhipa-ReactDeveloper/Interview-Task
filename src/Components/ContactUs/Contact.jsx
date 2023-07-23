a5819104d046222c0e418216a08f49c2e79eb8a0
Suraj Chhipa <SurajChhipa092002@gmail.com>
2023-07-23 15:53:14 +0530

    Text Area problem Chaked & Soluation pandding

--- src/Components/ContactUs/Contact.jsx
+++ src/Components/ContactUs/Contact.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "./Contact.css";
 import "./ContactMd.css";
-import {Icon} from "../../Constant/IconPath"
+import { Icon } from "../../Constant/IconPath";
 
 const ContactUs = () => {
+  // const TextArea = document.querySelector("textarea");
+  // TextArea.addEventListener("keyup", (e) => {
+  //   let TextHeight = e.target.scrollHeight;
+  //   TextArea.style.height = `${TextHeight}px`;
+  // });
+  const adjustTextareaHeight = (e) => {
+    e.target.style.height = "auto";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+  };
   return (
     <>
       <div className="Contact_Container">
         <div className="Contact_Main">
-        <img src={Icon.happy} alt="" className="Emoji"/>
+          <img src={Icon.happy} alt="" className="Emoji" />
           <div className="Contact_Left_Container">
             <div className="Contact_Heading">Send Us A Message</div>
             <div className="Contact_Input">
@@ -32,15 +41,21 @@ const ContactUs = () => {
                 />
               </div>
               <div className="Query_Input">
-                <input
+                <textarea
                   className="Query"
-                  type="text"
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="10"
                   placeholder="Type Your Query Here..."
-                />
+                  required
+                  onKeyUp={adjustTextareaHeight}
+                ></textarea>
               </div>
               <button className="SubmitBtn">Submit</button>
             </div>
           </div>
+          {/* Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi voluptatibus fugit repellendus error, quibusdam totam eius nemo! Enim dolor laborum quis fugiat aspernatur eum deleniti, corrupti eaque optio nam error. */}
           {/* <div className="Contact_Right_Container"></div> */}
         </div>
       </div>
